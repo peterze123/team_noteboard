f70c6a9e3c720f1fb43e2f2fe630d1eb446db88e
peterze123 <peteztc@gmail.com>
2021-08-19 00:10:27 -0400

    draggable enabled

--- models.py
+++ models.py
@@ -6,27 +6,26 @@ import sys
 class Tasks(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(50), nullable = True)
-    content = db.Column(db.String(200), nullable = False)
-    time = db.Column(db.String, nullable = False)
+    content = db.Column(db.String(2000), nullable = False)
+    time = db.Column(db.String, nullable = True)
     
     def __repr__(self):
         if self.name is not None:
-            return f'{self.content} for {self.name} created on {self.time}'
+            return f'{self.content} for {self.name} to be completed on {self.time}'
         else:
             return f'{self.content} created on {self.time}'
 # 
 def init_db():
     db.drop_all()
     db.create_all()
-    # Create a Test Case
-    t = Tasks(name = 'Peter', content = 'Get the job done !!!', time = '12:00')
+    # Create a sample Note for public board and personal board
+    t1 = Tasks(content = 'Start the day without any stress!!!')
+    t2 = Tasks(name= 'Peter', content = 'Start the day without any stress!!!', time = '12:00')
     try:
-        db.session.add(t)
+        db.session.add(t1)
+        db.session.add(t2)
         db.session.commit()
     except exc.OperationalError as e:
         sys.exit(e)
-    #
-    db.session.delete(t)
-    db.session.commit()
 
 init_db()
--- tasks.py
+++ tasks.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, redirect, render_template, url_for, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, validators, TextAreaField
+from wtforms.fields.html5 import TimeField
 from models import Tasks
 from app import db
 from datetime import datetime
@@ -10,8 +11,9 @@ bp = Blueprint('tasks', __name__)
 
 # adding tasks for the day
 class AddForm(FlaskForm):
-    task = TextAreaField('Task', [validators.length(max = 200), validators.DataRequired(message='please enter a task')], render_kw = {"rows": 5, "cols": 30})
+    task = TextAreaField('Task', [validators.length(max = 2000), validators.DataRequired(message='please enter a task')], render_kw = {"rows": 5, "cols": 30})
     name = StringField('Name', [validators.Length(max = 50), validators.Optional()])
+    time= TimeField('Time', [validators.Optional()], format='%H:%M', default=None)
     submit = SubmitField('OK')
 
 # delete tasks 
@@ -33,11 +35,15 @@ def create():
     if form.validate_on_submit():
         URL = url_for('tasks.board')
         #
-        if form.name.data:
-            t = Tasks(name = form.name.data, content = form.task.data, time = datetime.now().strftime('%H:%M'))
+        if form.name.data and form.time.data:
+            t = Tasks(name = form.name.data, content = form.task.data, time = str(form.time.data))
             URL = url_for('login.temp',check_name= form.name.data)
+        elif form.time.data:
+            t = Tasks(name = None, content = form.task.data, time = str(form.time.data))
+        elif form.name.data:
+            t = Tasks(name = form.name.data, content = form.task.data, time = None)
         else:
-            t = Tasks(name = None, content = form.task.data, time = datetime.now().strftime('%H:%M'))
+            t = Tasks(name = None, content = form.task.data, time = None)
         db.session.add(t)
         db.session.commit()
         #
