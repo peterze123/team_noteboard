e6e4f83c8b65486da63913ad8d33ac9e502ff7f2
peterze123 <peteztc@gmail.com>
2022-07-16 17:15:55 -0400

    aws lightsail

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@
 
 FROM python:3
 
-WORKDIR /python-docker
+WORKDIR /team-noteboard
 
 COPY requirements.txt requirements.txt
 RUN pip3 install -r requirements.txt
--- app.py
+++ app.py
@@ -32,4 +32,4 @@ app.add_url_rule('/', endpoint = 'tasks.board')
 
 # run the app
 if __name__== '__main__':
-    app.run(debug = True)
+    app.run(host='0.0.0.0',port=5000)
